74b92cc0c7edfa72580652e12642b544713a9b3f
Alf-caput <alfre.roble.aba@gmail.com>
2023-04-14 12:25:33 +0200

    plotting

--- kmedias_iter.m
+++ kmedias_iter.m
@@ -3,14 +3,14 @@ function [res_clusters, i] = kmedias_iter(x, cluster_est, tol)
     [k, p] = size(cluster_est); % k clústers de p coordenadas
     cluster = kmedias(x, cluster_est, n, k, p);
     i = 1;
-    res_clusters = [cluster_est; cluster];
+    res_clusters = cluster_est;
     dist = pdist2(cluster_est, cluster);
     dist = diag(dist);
     error = mean(dist);
     while error >= tol && i < 5
         cluster_est = cluster;
         cluster = kmedias(x, cluster_est, n, k, p);
-        res_clusters = [res_clusters; cluster];
+        res_clusters = [res_clusters; cluster_est];
         i = i+1;
 
         dist = pdist2(cluster_est, cluster);
